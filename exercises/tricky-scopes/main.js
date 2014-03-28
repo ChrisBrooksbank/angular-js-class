43ec28b64924825da9597d2ccad32c13a466d6ab
Chris Brooksbank <chrisbrooksbank@gmail.com>
2014-03-28 12:40:36 +0000

    done tricky scope excercise

--- exercises/tricky-scopes/main.js
+++ exercises/tricky-scopes/main.js
@@ -1,16 +1,17 @@
 var app = angular.module("exercise",[]);
 
 app.controller("main",function($scope) {
-  $scope.count = 0
+  $scope.model = {count: 0}
 })
 
 app.controller("child",function($scope) {
   $scope.increment = function() {
-    $scope.count += 1
+    $scope.model.count += 1
   }
   $scope.delayedIncrement = function() {
     setTimeout(function() {
-      $scope.count += 1
+      $scope.model.count += 1
+      $scope.$apply()
     },250); // just perceptible delay
   }
 })
