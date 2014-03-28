a7677bf4ffd39b56fb54c3d8660ec194839c7129
Chris Brooksbank <chrisbrooksbank@gmail.com>
2014-03-28 11:21:59 +0000

    did excercise 2

--- exercises/controllers-scopes/main.js
+++ exercises/controllers-scopes/main.js
@@ -4,13 +4,18 @@ var app = angular.module("exercise",[]);
 // we're missing a controller
 // - you'll see an error in the console that'll suggest what to fix.
 
+app.controller("defineMe",function($scope) {
+})
+
 app.controller("listful",function($scope) {
   // YOUR CODE
-  $scope.list = []; // looking a bit bare
+  $scope.list = [ {title:"fish"}, {title:"banana"} ]; // looking a bit bare
 })
 
 app.controller("tabs",function($scope) {
   // YOUR CODE
   // the view is expecting a function to be defined
   // on the scope - can you fill it in?
+  $scope.showTab = function(tabname) { 
+  	$scope.tab = tabname; }
 })
